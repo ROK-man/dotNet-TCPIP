b13b841b8d4ebb3abeae4e199e4f26475d343ea9
ROKman <hwangjs0823@pusan.ac.kr>
2024-11-02 20:09:51 +0900

    level1

---
+++ Sub/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Sub
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IPEndPoint ipEndPoint = new(IPAddress.Parse("127.0.0.1"), 25000);
+
+            Socket Client = new(ipEndPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            Client.Connect(ipEndPoint);
+            Console.WriteLine("Server Connected, serevr IP:" + ipEndPoint.Address.ToString());
+            Console.WriteLine("If you want to quit, send exit");
+
+            while (true)
+            {
+                // Send message.
+                Console.Write("Input: ");
+                var message = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(message))
+                {
+                    message = "null input";
+                }
+                var messageBytes = Encoding.UTF8.GetBytes(message);
+                _ = Client.Send(messageBytes, SocketFlags.None);
+
+                // Receive ack.
+                var buffer = new byte[1_024];
+                var received = Client.Receive(buffer, SocketFlags.None);
+                var response = Encoding.UTF8.GetString(buffer, 0, received);
+                Console.WriteLine(
+                    $"Socket client received: \"{response}\"");
+                if (response == "exit")
+                {
+                    Console.WriteLine("Closing Connect");
+                    break;
+                }
+            }
+
+            Client.Shutdown(SocketShutdown.Both);
+            Client.Close();
+        }
+    }
+}
---
+++ dotNet/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IPEndPoint ipEndPoint = new(IPAddress.Loopback, 25000);
+            Socket socket = new Socket(ipEndPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            socket.Bind(ipEndPoint);
+            socket.Listen(10); // Set a reasonable queue size
+            Console.WriteLine("Listening socket is waiting for a connection...");
+
+            var handler =  socket.Accept();
+            Console.WriteLine("Client connected: " + handler.RemoteEndPoint?.ToString());
+
+            var buffer = new byte[1024];
+            int received;
+            while ((received = handler.Receive(buffer, SocketFlags.None))>0)
+            {
+                var response = Encoding.UTF8.GetString(buffer, 0, received);
+
+                 Console.WriteLine($"Socket server received message: \"{response}\"");
+
+                var echoBytes = Encoding.UTF8.GetBytes(response);
+                handler.Send(echoBytes, SocketFlags.None);
+
+                if (response == "exit")
+                {
+                    Console.WriteLine("Client has sent Closing message");
+                    break; // Exit the loop after acknowledgment
+                }
+            }
+
+            // Close the handler and socket once done
+            handler.Shutdown(SocketShutdown.Both);
+            handler.Close();
+            socket.Close();
+        }
+    }
+}
