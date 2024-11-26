7382e13775d3e45d0ceb5019e4073cc7a6117741
Laptop <hwangjs0823@pusan.ac.kr>
2024-11-26 11:14:00 +0900

    LAPTOP

---
+++ 08. Chatting Application/CHattingApplication/Message.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChattingServer
+{
+    internal class Message
+    {
+        // 프로토콜 종류
+        public static readonly int ERROR = -1;
+        public static readonly int TEXT = 0;
+        public static readonly int GETFILELIST = 50;
+        public static readonly int GETFILE = 100;
+
+        public MessageHeader Header;
+        public MessagePayload Payload;
+
+        public const int HEADERLENGTH = 20;
+
+        public Message(int protocol, int textLength, int userID, string text)
+        {
+            this.Payload = new MessagePayload(text);
+            this.Header = new MessageHeader(protocol, textLength + 20, GetCheckSum(Payload.Text), userID);
+        }
+
+        public bool CheckHeader()
+        {
+            return this.Header.IsHeader();
+        }
+
+        public bool CompareCheckSum()
+        {
+            return this.Header.CheckSum == GetCheckSum(this.Payload.Text);
+        }
+
+        private static int GetCheckSum(string text)
+        {
+            if(String.IsNullOrEmpty(text))
+            {
+                return 0;
+            }
+
+            byte[] data = Encoding.UTF8.GetBytes(text);
+
+            int checksum = 0;
+            foreach (byte b in data)
+            {
+                checksum += b;
+            }
+            return checksum & 0x0FFFFFFF; // 4-byte checksum
+        }
+    }
+
+    // 크기가 20바이트 고정인 구조체
+    struct MessageHeader
+    {
+        public int Protocol;
+        public int TotalLength;
+        public int CheckSum;
+        public int UserID;
+        private const int Key = 0x12345678;
+
+        public MessageHeader(int protocol, int length, int sum, int userID)
+        {
+            Protocol = protocol;
+            TotalLength = length;
+            CheckSum = sum;
+            UserID = userID;
+        }
+
+        public bool IsHeader()
+        {
+            return Key == 0x12345678;
+        }
+    }
+
+
+    class MessagePayload(string text)
+    {
+        public string Text { get; set; } = text;
+    }
+
+
+}
---
+++ 08. Chatting Application/ChattingServer/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace ChattingServer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter \"START\" for running server");
+            Console.WriteLine("Enter \"STOP\" for terminate server");
+            Console.WriteLine("Enter \"CHECK\" for see how many people connected");
+            Console.WriteLine("Enter \"QUIT\" for terminate program");
+
+            string input;
+            while(true)
+            {
+                input = Console.ReadLine() ?? "";
+                if(input.ToLower().Equals("start"))
+                {
+
+                }
+                else if(input.ToLower().Equals("stop"))
+                {
+                    Console.WriteLine("Are you sure to shutdown server?  ['p']");
+                    input = Console.ReadLine() ?? "";
+                    if (input.Equals("q"))
+                    {
+                        Console.WriteLine("OK, bye");
+                        return;
+                    }
+                }
+                else if(input.ToLower().Equals("check"))
+                {
+
+                }
+                else if(input.ToLower().Equals("quit"))
+                {
+                    Console.WriteLine("Are you sure to quit?  ['q']");
+                    input = Console.ReadLine() ?? "";
+                    if (input.Equals("q"))
+                    {
+                        Console.WriteLine("OK, bye");
+                        return;
+                    }
+                }
+                else
+                {
+                    continue;
+                }
+            }
+        }
+    }
+}
